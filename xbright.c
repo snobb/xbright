1e262fd84b6ac5cb26104a69e7dae5e46b91eb8c
Alex Kozadaev <akozadaev at yahoo com>
2023-12-23 22:32:28 +0000

    Lock the brightness range to 0-10.
    WARNING: This maybe a breaking change for some as previously the tool
    used the devise brightness range, which may be very high (eg. I've seen
    >3mil), which makes default "+" option rather pointless and one needs to figure the offset. This change solves this by computing a step value for the given device so that there is always 10 steps between min and max brightness.

--- xbright.c
+++ xbright.c
@@ -1,6 +1,6 @@
 /*
  *  xbright.c
- *  author: Alex Kozadaev (2011-2021)
+ *  author: Alex Kozadaev (2011-Present)
  */
 
 #include <stdio.h>
@@ -12,26 +12,28 @@
 #define NAME "xbright"
 
 #define ERROR(msg)      { fprintf(stderr, "ERROR: %s - %s\n", __func__, \
-                            (msg)); exit(EXIT_FAILURE); } while(0)
+                                      (msg)); exit(EXIT_FAILURE); } while(0)
 #ifdef DEBUG
-#define DBG(fmt, ...) fprintf(stderr, fmt "\n", __VA_ARGS__);
+    #define DBG(fmt, ...) fprintf(stderr, fmt "\n", __VA_ARGS__);
 #else
-#define DBG(fmt, ...)
+    #define DBG(fmt, ...)
 #endif
 
 #define MIN(a, b)       ((a) < (b) ? (a) : (b))
 #define MAX(a, b)       ((a) > (b) ? (a) : (b))
 
-static void bright_up(const char*);
-static void bright_down(const char*);
-static void bright_set(const char*);
+const int STEPS = 10;
+#define TOSTEPS(a)      ((int)((float)a / STEPS))
+const int STEP = MAX(TOSTEPS(MAXVALUE), 1);
+
+static void bright_up(const char *);
+static void bright_down(const char *);
+static void bright_set(const char *);
 static int get_current(void);
 static void commit_change(int);
 static void usage(void);
 
-int
-main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     if (geteuid()) {
         usage();
         ERROR("Must be root");
@@ -39,7 +41,7 @@ main(int argc, char **argv)
     }
 
     if (argc > 1) {
-        switch(*argv[1]) {
+        switch (*argv[1]) {
             case '+': bright_up(++argv[1]); break;
             case '-': bright_down(++argv[1]); break;
             case '=': bright_set(++argv[1]); break;
@@ -51,42 +53,34 @@ main(int argc, char **argv)
     return EXIT_SUCCESS;
 }
 
-void
-bright_up(const char *value)
-{
+void bright_up(const char *value) {
     int offset_value = MAX(atoi(value), 1);
     int cur_value = get_current();
-    int new_value = MIN(cur_value + offset_value, MAXVALUE);
+    int new_value = MIN(cur_value + (offset_value * STEP), MAXVALUE);
 
     if (new_value != cur_value) {
         commit_change(new_value);
     }
 }
 
-void
-bright_down(const char *value)
-{
+void bright_down(const char *value) {
     int offset_value = MAX(atoi(value), 1);
     int cur_value = get_current();
-    int new_value = MAX(cur_value - offset_value, 0);
+    int new_value = MAX(cur_value - (offset_value * STEP), 0);
 
     if (new_value != cur_value) {
         commit_change(new_value);
     }
 }
 
-void
-bright_set(const char *value)
-{
-    int set_value = MIN(atoi(value), MAXVALUE);
-    commit_change(set_value);
+void bright_set(const char *value) {
+    int set_value = MIN(atoi(value), STEPS);
+    commit_change(set_value * STEP);
 }
 
-int
-get_current(void)
-{
+int get_current(void) {
     int value = 0;
-    FILE* input = fopen(BRIGHTNESSFILE, "r");
+    FILE *input = fopen(BRIGHTNESSFILE, "r");
     if (input == NULL) {
         ERROR("Cannot open kernel pipe");
     }
@@ -95,10 +89,8 @@ get_current(void)
     return value;
 }
 
-void
-commit_change(int value)
-{
-    FILE* output = fopen(BRIGHTNESSFILE, "w");
+void commit_change(int value) {
+    FILE *output = fopen(BRIGHTNESSFILE, "w");
     if (output == NULL) {
         ERROR("Cannot open kernel pipe");
     }
@@ -107,9 +99,7 @@ commit_change(int value)
     fclose(output);
 }
 
-void
-usage(void)
-{
+void usage(void) {
     printf("%s v%s (works only with 2.6+ kernels).\n", NAME, VERSION);
-    printf("usage: %s [+-=[0-%d]]\n", NAME, MAXVALUE);
+    printf("usage: %s [+-=[0-%d]]\n", NAME, STEPS);
 }
